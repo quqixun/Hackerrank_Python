6cb9f406a8a0756917a1ab6b0c54186c9ba5ddde
Qixun Qu <quqixun@gmail.com>
2017-11-27 20:26:54 +0100

    Upload code for day15

---
+++ 30 Days of Code/day15_linked_list.py
@@ -0,0 +1,32 @@
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None 
+class Solution: 
+    def display(self,head):
+        current = head
+        while current:
+            print(current.data,end=' ')
+            current = current.next
+
+    def insert(self,head,data): 
+    #Complete this method
+        node = Node(data)
+        temp = head
+        if temp is None:
+            return node
+        else:
+            while(True):
+                if temp.next is None:
+                    temp.next = node
+                    break
+                temp = temp.next
+        return head
+
+mylist= Solution()
+T=int(input())
+head=None
+for i in range(T):
+    data=int(input())
+    head=mylist.insert(head,data)    
+mylist.display(head);
