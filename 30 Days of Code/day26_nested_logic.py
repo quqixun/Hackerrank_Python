bb85917e84721583d8af2652abdcfe19b5c400dd
Qixun Qu <quqixun@gmail.com>
2017-12-19 23:05:34 +0100

    Modify conditions on months

--- 30 Days of Code/day26_nested_logic.py
+++ 30 Days of Code/day26_nested_logic.py
@@ -1,13 +1,18 @@
+#!/bin/python3
+
+import sys
+
+
 ad, am, ay = map(int, input().strip().split(" "))
 ed, em, ey = map(int, input().strip().split(" "))
 
 hackos = 0
 
 if ay == ey:
-    if am <= em:
+    if am == em:
         if ad > ed:
             hackos = 15 * (ad - ed)
-    else:
+    elif am > em:
         hackos = 500 * (am - em)
 elif ay > ey:
     hackos = 10000
